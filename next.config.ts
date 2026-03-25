ddcc793f1d6f81498c2cc5fdea3591ffa80319f0
Andrew Valleteau <avallete@users.noreply.github.com>
2026-03-25 10:50:52 +0100

    wip

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@electric-sql/pglite"],
+  allowedDevOrigins: ['100.102.46.17', '192.168.1.18'],
 };
 
 export default nextConfig;
